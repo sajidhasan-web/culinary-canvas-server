c0e9a2ad4f5ef901f35437d36cb106c28a000696
sajidhasan-web <sajidhasan.web@gmail.com>
2024-05-12 00:55:14 +0600

    adding single food data fetching route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 9000;
 
@@ -21,7 +22,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rsmyn0p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -36,8 +37,20 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+    const foodsCollection = client.db('CulinaryCanvasDB').collection('foods')
 
+
+    app.get('/foods', async(req, res) => {
+        const result = await foodsCollection.find().toArray();
+        res.send(result);
+    })
+
+    app.get("/food/:id", async(req, res) => {
+        const id = req.params.id;
+        const result = await foodsCollection.findOne({_id: ObjectId(id)});
+        res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
