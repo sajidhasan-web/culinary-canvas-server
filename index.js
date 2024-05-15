47ba308d18d3f69d6cac4cb3e20d2a2cf7b88ca8
sajidhasan-web <sajidhasan.web@gmail.com>
2024-05-15 14:50:29 +0600

    add food api adding to the serverside

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 9000;
 
-
-
-const corsOptions ={
-    origin: [
-        'http://localhost:5173',
-        'http://localhost:5174',
-      ],
-      credentials: true,
-      optionSuccessStatus: 200,
-}
-
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
 // middleware
 app.use(cors(corsOptions));
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rsmyn0p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -31,72 +23,113 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const foodsCollection = client.db('CulinaryCanvasDB').collection('foods')
-    const purchasesCollection = client.db('CulinaryCanvasDB').collection('purchases')
+    const foodsCollection = client.db("CulinaryCanvasDB").collection("foods");
+    const purchasesCollection = client
+      .db("CulinaryCanvasDB")
+      .collection("purchases");
+    const feedbackCollection = client
+      .db("CulinaryCanvasDB")
+      .collection("feedback");
 
-
-    app.get('/foods', async(req, res) => {
-        const result = await foodsCollection.find().toArray();
-        res.send(result);
-    })
-
-    app.get("/food/:id", async(req, res) => {
-        const id = req.params.id;
-        const result = await foodsCollection.findOne({_id: new ObjectId(id)});
-        res.send(result);
-    })
-
-    app.get("/search", async(req, res) => {
-      const { query } = req.query;
-      const result = await foodsCollection.find({ name: { $regex: query, $options: 'i' } }).toArray();
+    app.get("/foods", async (req, res) => {
+      const result = await foodsCollection.find().toArray();
       res.send(result);
-  });
+    });
 
-
-  app.post("/purchases", async(req, res)=>{
-    try {
-        const { name, price, quantity, buyerName, buyerEmail, buyingDate } = req.body;
-        const purchase = {
-            name,
-            price, 
-            quantity,
-            buyerName,
-            buyerEmail,
-            buyingDate: new Date(buyingDate)
+    app.post("/add-food", async (req, res) => {
+      try {
+        const {name, image, price,  quantity,  origin,  description,  userEmail,  userName } = req.body;
+        const food = {
+          name,
+          price,
+          image,
+          quantity,
+          origin,
+          description,
+          userEmail,
+          userName,
         };
-        const result = await purchasesCollection.insertOne(purchase)
+        const result = await foodsCollection.insertOne(food);
         res.json(result);
-    } catch (error) {
-        console.error('Error storing purchase:', error);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-});
+      } catch (error) {
+        console.error("Error storing food:", error);
+        res.status(500).json({ message: "Internal server error" });
+      }
+    });
 
-      
-app.patch("/foods/:id", async(req, res)=>{
-  try {
-      const { id } = req.params;
-      const { quantity } = req.body;
-      await foodsCollection.updateOne({ _id: new ObjectId(id) }, { $inc: { quantity: -quantity } });
-      res.json({ message: 'Quantity updated successfully' });
-  } catch (error) {
-      console.error('Error updating quantity:', error);
-      res.status(500).json({ message: 'Internal server error' });
-  }
-});
+    app.get("/food/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await foodsCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    app.get("/my-added-food/:email", async (req, res) => {
+      try {
+        const email = req.params.email;
+        const result = await foodsCollection.find({ userEmail: email }).toArray();
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("An error occurred while fetching data.");
+      }
+    });
     
-  
+
+    app.get("/search", async (req, res) => {
+      const { query } = req.query;
+      const result = await foodsCollection
+        .find({ name: { $regex: query, $options: "i" } })
+        .toArray();
+      res.send(result);
+    });
+
+    app.post("/purchases", async (req, res) => {
+      try {
+        const { name, price, quantity, buyerName, buyerEmail, buyingDate } =
+          req.body;
+        const purchase = {
+          name,
+          price,
+          quantity,
+          buyerName,
+          buyerEmail,
+          buyingDate: new Date(buyingDate),
+        };
+        const result = await purchasesCollection.insertOne(purchase);
+        res.json(result);
+      } catch (error) {
+        console.error("Error storing purchase:", error);
+        res.status(500).json({ message: "Internal server error" });
+      }
+    });
+
+    app.patch("/foods/:id", async (req, res) => {
+      try {
+        const { id } = req.params;
+        const { quantity } = req.body;
+        await foodsCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $inc: { quantity: -quantity } }
+        );
+        res.json({ message: "Quantity updated successfully" });
+      } catch (error) {
+        console.error("Error updating quantity:", error);
+        res.status(500).json({ message: "Internal server error" });
+      }
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -104,11 +137,10 @@ app.patch("/foods/:id", async(req, res)=>{
 }
 run().catch(console.dir);
 
-
 app.get("/", (req, res) => {
-    res.send("CulinaryCanvas server is running");
-  });
-  
-  app.listen(port, () => {
-    console.log(`CulinaryCanvas server is running on port ${port}`);
-  });
+  res.send("CulinaryCanvas server is running");
+});
+
+app.listen(port, () => {
+  console.log(`CulinaryCanvas server is running on port ${port}`);
+});
